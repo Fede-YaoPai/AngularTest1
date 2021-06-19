73b57ce16bf4c2610ae23a21cd1ca9ceb6774980
Fede-YaoPai <federico.lecca@hotmail.com>
2021-06-19 12:50:48 +0200

    boooh

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,28 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LayoutComponent } from './core/layout/layout.component';
+import { LoginModule } from './features/login/login.module';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '', 
+    component: LayoutComponent,
+    children: [
+      {
+        path: '',
+        loadChildren: () => import("./features/home/home.module").then(m => m.HomeModule)
+      },
+      {
+        path: 'login',
+        loadChildren: () => import("./features/login/login.module").then(m => LoginModule)
+      },
+      {
+        path: 'info',
+        loadChildren: () => import("./features/info/info.module").then(m => m.InfoModule)
+      }
+    ]  
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
