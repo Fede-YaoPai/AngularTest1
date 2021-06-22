29dffc8b2e59febc4cf63091b620bcd700598cdd
Fede-YaoPai <federico.lecca@hotmail.com>
2021-06-22 17:32:51 +0200

    ok

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,29 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-<<<<<<< HEAD
-import { LayoutComponent } from './core/layout/layout.component';
-import { LoginModule } from './features/login/login.module';
 
-const routes: Routes = [
-  {
-    path: '', 
-    component: LayoutComponent,
-    children: [
-      {
-        path: '',
-        loadChildren: () => import("./features/home/home.module").then(m => m.HomeModule)
-      },
-      {
-        path: 'login',
-        loadChildren: () => import("./features/login/login.module").then(m => LoginModule)
-      },
-      {
-        path: 'info',
-        loadChildren: () => import("./features/info/info.module").then(m => m.InfoModule)
-      }
-    ]  
-  }
-=======
 import {LayoutComponent} from "./core/layout/layout.component";
 
 const routes: Routes = [
@@ -40,7 +17,6 @@ const routes: Routes = [
     ]
 
     }
->>>>>>> features/ale
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,15 @@
 import {NgModule} from '@angular/core';
 import {BrowserModule} from '@angular/platform-browser';
 
-<<<<<<< HEAD
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { LoginComponent } from './features/login/login.component';
-=======
 import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
 import {HomeModule} from "./features/home/home.module";
 import {LayoutModule} from "./core/layout/layout.module";
->>>>>>> features/ale
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
   ],
   imports: [
     BrowserModule,
